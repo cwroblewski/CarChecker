a013efd7b4b163b303f22ffc531493da4643bc44
Cezary Wróblewski <c.wroblewski@il-pib.pl>
2020-11-23 16:37:53 +0100

    fix

--- cars/api_views.py
+++ cars/api_views.py
@@ -21,20 +21,20 @@ class CarViewSet(viewsets.ModelViewSet):
         connector = ExternalApiConnector(make=str(self.request.POST.get('make')).lower(),
                                          vehicle_model=str(self.request.POST.get('model')).lower())
 
-        if connector.check_model_for_make() is not bool:
-            status_code = connector.check_model_for_make()
-
-            return Response(status=status_code, data={'message': f'External API responded with a status of {status_code}'})
-
-        elif connector.check_model_for_make():
+        if connector.check_model_for_make():
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-        else:
+        elif not connector.check_model_for_make():
             return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Not found'})
+
+        else:
+            status_code = connector.check_model_for_make()
+
+            return Response(status=status_code, data={'message': f'External API responded with a status of {status_code}'})
 
     def retrieve(self, request, *args, **kwargs):
         response = {'detail': 'Retrieve not allowed.'}
