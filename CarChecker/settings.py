e7781d29b2eded1fd79ea930df911afa28b281cd
Cezary Wróblewski <c.wroblewski@il-pib.pl>
2020-11-23 12:58:35 +0100

    heroku.yml

--- CarChecker/settings.py
+++ CarChecker/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9h_uiku_rju(90$$-7tpinj-^^0lm&8ow5$rq^vjh7@*&8ts@5'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'powerful-oasis-83097.herokuapp.com' ]
 
